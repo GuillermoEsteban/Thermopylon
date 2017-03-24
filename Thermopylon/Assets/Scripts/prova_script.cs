41d3d19b8ca8345d7361eae64481e5b624a24c54
Guillem Serra <guillemserralorenz@gmail.com>
2017-03-24 23:02:07 +0100

    prova summary
    prova description

--- Thermopylon/Assets/Scripts/prova_script.cs
+++ Thermopylon/Assets/Scripts/prova_script.cs
@@ -7,7 +7,8 @@ public class prova_script : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Debug.Log("Hello World!");
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
