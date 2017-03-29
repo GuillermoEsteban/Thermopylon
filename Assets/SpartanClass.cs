6e79ab9f6b23fba3c2cb414760137aec9b117641
Unknown <guillermoestebanplazas@enti.cat>
2017-03-29 11:16:42 +0200

    trhthth
    htrhyhyh

--- Assets/SpartanClass.cs
+++ Assets/SpartanClass.cs
@@ -1,15 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class SpartanClass : MonoBehaviour {
 
     // Use this for initialization
     float xPos,yPos;
+    float[] direction;
+    float speed;
 
-	void Start ()
+
+    void Start ()
     {
-	 
-	}
+        direction = new float[2];
+        speed = 0.5f;
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -22,6 +27,10 @@ public class SpartanClass : MonoBehaviour {
 
     void moveToPosition(float x, float y)
     {
+        float modulo;
+        direction[0] = x - xPos;
+        direction[1] = y - yPos;
+        modulo = (float)Math.Sqrt(Math.Pow(direction[0],2)+ Math.Pow(direction[1],2));
 
     }
 }
