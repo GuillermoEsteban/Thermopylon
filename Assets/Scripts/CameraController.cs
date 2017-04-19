8440b80e23659e356ef4b8e3d074e5da09ce3a23
Guillem Serra <guillemserralorenz@gmail.com>
2017-04-19 19:41:41 +0200

    zoom de la camera + background provisional

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public static Camera cam;
+    private int zoomSpeed = 5;
+    private static int maxZoom = 10;
+    private static int minZoom = 60;
+
+
+    // Use this for initialization
+    void Start() {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetAxis("Mouse ScrollWheel") < 0) // back
+        {
+            cam.orthographicSize+=zoomSpeed;
+            if(cam.orthographicSize>60)
+            {
+                cam.orthographicSize = minZoom;
+            }
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") >0) // forward
+        {
+            cam.orthographicSize-=zoomSpeed;
+            if(cam.orthographicSize<10)
+            {
+                cam.orthographicSize = maxZoom;
+            }
+        }
+    }
+}
