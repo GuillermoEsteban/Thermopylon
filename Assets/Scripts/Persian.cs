8b593966638937e3316dad6a38847616b98799d8
Guillem Serra <guillemserralorenz@gmail.com>
2017-04-25 04:14:25 +0200

    avançant amb persa + henomotias

--- Assets/Scripts/Persian.cs
+++ Assets/Scripts/Persian.cs
@@ -3,14 +3,115 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Persian : MonoBehaviour {
+    //Canvi de sprites
+    public Animator anim;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    // Use this for initialization
+    void Start () {
+        //creem la variable animació per més comoditat:
+        anim = GetComponent<Animator>();
+
+        anim.SetBool("moving", false);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    //funció per canviar la imatge de l'sprite segons l'angle:
+    public void changeSprite(float angle, float posY)
+    {
+        if (anim.GetBool("moving")){ 
+            if (angle < 22.5f)
+            {
+                anim.SetBool("walk_1", false);
+                anim.SetBool("walk_2", false);
+                anim.SetBool("walk_3", true);
+                anim.SetBool("walk_4", false);
+                anim.SetBool("walk_5", false);
+                anim.SetBool("walk_6", false);
+                anim.SetBool("walk_7", false);
+                anim.SetBool("walk_8", false);
+            }
+            else if (22.5f <= angle && angle < 67.5f && posY > 0)
+            {
+                anim.SetBool("walk_1", false);
+                anim.SetBool("walk_2", false);
+                anim.SetBool("walk_3", false);
+                anim.SetBool("walk_4", true);
+                anim.SetBool("walk_5", false);
+                anim.SetBool("walk_6", false);
+                anim.SetBool("walk_7", false);
+                anim.SetBool("walk_8", false);
+            }
+            else if (67.5f <= angle && angle < 112.5f && posY > 0)
+            {
+
+                anim.SetBool("walk_1", false);
+                anim.SetBool("walk_2", false);
+                anim.SetBool("walk_3", false);
+                anim.SetBool("walk_4", false);
+                anim.SetBool("walk_5", true);
+                anim.SetBool("walk_6", false);
+                anim.SetBool("walk_7", false);
+                anim.SetBool("walk_8", false);
+            }
+            else if (112.5f <= angle && angle < 157.5f && posY > 0)
+            {
+                anim.SetBool("walk_1", false);
+                anim.SetBool("walk_2", false);
+                anim.SetBool("walk_3", false);
+                anim.SetBool("walk_4", false);
+                anim.SetBool("walk_5", false);
+                anim.SetBool("walk_6", true);
+                anim.SetBool("walk_7", false);
+                anim.SetBool("walk_8", false);
+            }
+            else if (angle > 157.5f)
+            {
+                anim.SetBool("walk_1", false);
+                anim.SetBool("walk_2", false);
+                anim.SetBool("walk_3", false);
+                anim.SetBool("walk_4", false);
+                anim.SetBool("walk_5", false);
+                anim.SetBool("walk_6", false);
+                anim.SetBool("walk_7", true);
+                anim.SetBool("walk_8", false);
+            }
+            else if (22.5f <= angle && angle < 67.5f && posY < 0)
+            {
+                anim.SetBool("walk_1", false);
+                anim.SetBool("walk_2", true);
+                anim.SetBool("walk_3", false);
+                anim.SetBool("walk_4", false);
+                anim.SetBool("walk_5", false);
+                anim.SetBool("walk_6", false);
+                anim.SetBool("walk_7", false);
+                anim.SetBool("walk_8", false);
+            }
+            else if (67.5f <= angle && angle < 112.5f && posY < 0)
+            {
+
+                anim.SetBool("walk_1", true);
+                anim.SetBool("walk_2", false);
+                anim.SetBool("walk_3", false);
+                anim.SetBool("walk_4", false);
+                anim.SetBool("walk_5", false);
+                anim.SetBool("walk_6", false);
+                anim.SetBool("walk_7", false);
+                anim.SetBool("walk_8", false);
+            }
+            else if (112.5f <= angle && angle < 157.5f && posY < 0)
+            {
+                anim.SetBool("walk_1", false);
+                anim.SetBool("walk_2", false);
+                anim.SetBool("walk_3", false);
+                anim.SetBool("walk_4", false);
+                anim.SetBool("walk_5", false);
+                anim.SetBool("walk_6", false);
+                anim.SetBool("walk_7", false);
+                anim.SetBool("walk_8", true);
+            }
+        }
+    }
 }
